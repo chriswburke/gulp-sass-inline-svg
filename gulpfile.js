db51a49af33b0edd43eb78f587f67260b18e0e93
Chris W. Burke <chris@chriswburke.com>
2020-04-11 14:53:43 -0400

    Updating tests and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@
 var gulp = require('gulp');
 var svgmin = require('gulp-svgmin');
 var sass = require('gulp-sass');
-var inlinesvg = require('../index.js');
+var inlinesvg = require('./index.js');
 
 gulp.task('icons', function(){
-  return gulp.src('icons/**/*.svg')
+  return gulp.src('tests/icons/**/*.svg')
     .pipe(svgmin())
     .pipe(inlinesvg());
 })
 
 gulp.task('sass', gulp.series('icons', function(){
-  return gulp.src('test.css')
+  return gulp.src('tests/test.css')
     .pipe(sass())
     .pipe(gulp.dest('.'));
 }))
